2c80903f23ee77e81fb93d927fd2f5d1dc13d75f
kabangast <kabangast@gmail.com>
2024-11-23 03:24:11 +0300

    feat: Replace Hello World with example.com WebView
    Changes:
    - Modified MainActivity to use WebView instead of TextView
    - Added INTERNET permission to AndroidManifest.xml
    - Enabled JavaScript in WebView
    - Added WebViewClient to prevent external browser opening
    
    This change converts the app from a static Hello World display
    to a web browser that loads example.com within the app.

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -2,18 +2,17 @@ package com.example.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.graphics.Color;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("Hello World!");
-        textView.setTextSize(24); // Make text bigger
-        textView.setTextColor(Color.BLACK); // Set text color
-        textView.setPadding(20, 20, 20, 20); // Add some padding
-        setContentView(textView);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient()); // This line prevents opening in external browser
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://example.com");
+        setContentView(webView);
     }
 }
